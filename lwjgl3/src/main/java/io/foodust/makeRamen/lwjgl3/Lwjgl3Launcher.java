e63d0b89a217f482f9989fc724b4c4b67079bc08
Foodust <77597986+Foodust@users.noreply.github.com>
2024-12-10 21:54:57 +0900

    [feat] version and window

--- lwjgl3/src/main/java/io/foodust/makeRamen/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/foodust/makeRamen/lwjgl3/Lwjgl3Launcher.java
@@ -1,11 +1,16 @@
 package io.foodust.makeRamen.lwjgl3;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.foodust.makeRamen.game.MakeRamen;
 import io.foodust.makeRamen.object.ObjectManager;
 
-/** Launches the desktop (LWJGL3) application. */
+import java.awt.*;
+
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
@@ -18,11 +23,22 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+
+        // 모니터의 해상도 가져오기
+        Graphics.DisplayMode primaryMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+
+        // 창이 화면 중앙에 오도록 위치 계산
+        int windowX = (primaryMode.width - ObjectManager.X.intValue()) / 2;
+        int windowY = (primaryMode.height - ObjectManager.Y.intValue()) / 2;
+
         configuration.setTitle("우왁굳의 쌀국수 가게");
         configuration.useVsync(true);
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        configuration.setWindowedMode(ObjectManager.X.intValue(), ObjectManager.Y.intValue());
+        configuration.setWindowedMode(ObjectManager.X.intValue(),ObjectManager.Y.intValue());
+        configuration.setWindowPosition(windowX, windowY +100);
         configuration.setWindowIcon("logo128.png", "logo64.png", "logo32.png", "logo16.png");
+        configuration.setResizable(true);
+        configuration.setDecorated(true);
         return configuration;
     }
 }
